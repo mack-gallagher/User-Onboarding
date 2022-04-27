a1f577a89b041a1aa47c4a5020c212bdb1e89117
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-27 13:07:43 -0700

    move state handling to App component

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,13 +1,61 @@
 import './App.css';
+import { useState } from 'react';
 import Form from './Form.js';
 import formSchema from './validation/formSchema.js';
+import * as yup from 'yup';
+import axios from 'axios';
 
 function App() {
+
+  const initialFormValues = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    tos: false,
+  }
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [responses, setResponses] = useState([]);
+
+  const [users, setUsers] = useState([]);
+
+  const validate = () => {
+    return;
+  }
+
+  const sendPost = () => {
+    return;
+  }
+ 
+  const onChange = evt => {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    setFormValues({ ...formValues, [name]: value});
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault();
+    console.log(evt);
+    setFormValues(initialFormValues);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hello</div>
-        <Form />
+        <h4>Current Users:</h4>
+        <pre>
+          { responses.map(response => {
+              return JSON.stringify(response) 
+                }) }
+        </pre>
+        <Form
+          initialFormValues={initialFormValues}
+          formValues={formValues}
+          setFormValues={setFormValues}
+          onChange={onChange}
+          onSubmit={onSubmit}
+        />
       </header>
     </div>
   );
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,26 +1,63 @@
+import { useState } from 'react';
+
 export default function Form(props) {
+
+  const {
+          initialFormValues,
+          formValues,
+          setFormValues, 
+          onChange,
+          onSubmit,
+        } = props;
+
   return (
     <div className="form">
-      <form>
+      <form onSubmit={onSubmit}>
         <label>
           First Name:
-          <input name="first" type="text" /> 
+          <input 
+            name="first_name" 
+            type="text"
+            value={formValues.first_name}
+            onChange={onChange} 
+          /> 
         </label>
         <label>
           Last Name:
-          <input name="last" type="text" />
+          <input 
+            name="last_name" 
+            type="text"
+            value={formValues.last_name}
+            onChange={onChange} 
+          />
         </label>
         <label>
           Email:
-          <input name="email" type="text" />
+          <input 
+            name="email" 
+            type="text"
+            value={formValues.email}
+            onChange={onChange}
+          />
         </label>
         <label>
           Password:
-          <input name="password" type="text" />
+          <input 
+            name="password" 
+            type="text"
+            value={formValues.password}
+            onChange={onChange}
+          />
         </label>
         <label>
           I agree to the Terms of Service
-          <input name="tos" type="checkbox" />
+          <input 
+            name="tos"
+            type="checkbox"
+            value="tos"
+            checked={formValues.tos}
+            onChange={onChange}
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
